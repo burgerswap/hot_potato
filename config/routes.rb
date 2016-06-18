e2a721271f0b3244a364dfdc9676876313f0cd89
Aswin Radhakrishnan <aswinrkrishnan@gmail.com>
2016-06-18 20:45:28 +1000

    adding facebook omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :consumptions
-  devise_for :users
+  devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
   resources :leftovers
   root to: "homepage#index"
 end
